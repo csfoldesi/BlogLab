8cbcd10565048b87bb33b44831e933a45f7a93a2
Csaba Földesi <csabus@gmail.com>
2023-02-27 22:25:16 +0100

    AccountRepository created

---
+++ BlogLab.Repository/IAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿using BlogLab.Models.Account;
+using Microsoft.AspNet.Identity;
+
+namespace BlogLab.Repository
+{
+    public interface IAccountRepository
+    {
+        public Task<IdentityResult> CreateAsync(ApplicationUserIdentity user, CancellationToken cancellationToken);
+
+        public Task<ApplicationUserIdentity> GetByUsernameAsync(string normalizedUSername, CancellationToken cancellationToken);
+    }
+}
