2bbfd8d176e42d7808f30501f2bfc0a0e245a4f3
Csaba Földesi <csabus@gmail.com>
2023-03-07 19:11:54 +0100

    Program startup settings and initializations

--- BlogLab.Web/Program.cs
+++ BlogLab.Web/Program.cs
@@ -1,4 +1,14 @@
+using BlogLab.Identity;
+using BlogLab.Models.Account;
 using BlogLab.Models.Settings;
+using BlogLab.Repository;
+using BlogLab.Services;
+using BlogLab.Web.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace BlogLab.Web
 {
@@ -6,26 +16,89 @@ namespace BlogLab.Web
     {
         public static void Main(string[] args)
         {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+
             builder.Services.Configure<CloudinaryOptions>(builder.Configuration.GetSection("CloudinaryOptions"));
+
+            builder.Services.AddScoped<ITokenService, TokenService>();
+            builder.Services.AddScoped<IPhotoService, PhotoService>();
+
+            builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+            builder.Services.AddScoped<IBlogCommentRepository, BlogCommentRepository>();
+            builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+            builder.Services.AddScoped<IPhotoRepository, PhotoRepository>();
+
+            builder.Services
+                .AddIdentityCore<ApplicationUserIdentity>(opt =>
+                {
+                    opt.Password.RequireNonAlphanumeric = false;
+                })
+                .AddUserStore<UserStore>()
+                .AddDefaultTokenProviders()
+                .AddSignInManager<SignInManager<ApplicationUserIdentity>>();
+
+            builder.Services.AddControllers();
+            builder.Services.AddCors();
+
+            builder.Services
+                .AddAuthentication(opt =>
+                {
+                    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(opt =>
+                {
+                    opt.RequireHttpsMetadata = false;
+                    opt.SaveToken = true;
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Error");
+                app.UseDeveloperExceptionPage();
+                app.UseCors(opt => opt.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            } else
+            {
+                //app.UseExceptionHandler("/Error");
+                app.UseCors();
             }
-            app.UseStaticFiles();
+
+            app.ConfigureExceptionHandler();
 
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseAuthorization();
 
-            app.MapRazorPages();
+            app.UseEndpoints(endpoins =>
+            {
+                endpoins.MapControllers();
+            });
+
+
+            //app.UseStaticFiles();
+
+            //app.MapRazorPages();
 
             app.Run();
         }
