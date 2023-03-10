13dcf4019a6c92b11450a1e0f750a8996752ec5f
Csaba Földesi <csabus@gmail.com>
2023-03-10 20:31:27 +0100

    BlogController created

---
+++ BlogLab.Web/Controllers/BlogController.cs
@@ -0,0 +1,95 @@
+﻿using BlogLab.Models.Blog;
+using BlogLab.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace BlogLab.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly IBlogRepository _blogRepository;
+        private readonly IPhotoRepository _photoRepository;
+
+        public BlogController(IBlogRepository blogRepository, IPhotoRepository photoRepository)
+        {
+            _blogRepository = blogRepository;
+            _photoRepository = photoRepository;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<Blog>> Create(BlogCreate blogCreate)
+        {
+            var applicationUserId = int.Parse(User.Claims.First(i => i.Type == JwtRegisteredClaimNames.NameId).Value);
+
+            if(blogCreate.PhotoId.HasValue)
+            {
+                var photo = await _photoRepository.GetAsync(blogCreate.PhotoId.Value);
+                if (photo != null)
+                {
+                    if(photo.ApplicationUserId != applicationUserId) {
+                        return BadRequest("You did not upload the photo");
+                    }
+                } else
+                {
+                    return BadRequest("Photo not found");
+                }
+            }
+
+            var blog = await _blogRepository.UpsertAsync(blogCreate, applicationUserId);
+            return Ok(blog);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PagedResult<Blog>>> GetAll([FromQuery] BlogPaging blogPaging)
+        {
+            var blogs = await _blogRepository.GetAllAsync(blogPaging);
+            return Ok(blogs);
+        }
+
+        [HttpGet("{blogId}")]
+        public async Task<ActionResult<Blog>> Get(int blogId)
+        {
+            var blog = await _blogRepository.GetAsync(blogId);
+            return Ok(blog);
+        }
+
+        [HttpGet("user/{applicationUserId}")]
+        public async Task<ActionResult<List<Blog>>> GetByApplicationUserId(int applicationUserId)
+        {
+            var blogs = await _blogRepository.GetAllByUserIdAsync(applicationUserId);
+            return Ok(blogs);
+        }
+
+        [HttpGet("famous")]
+        public async Task<ActionResult<List<Blog>>> GetAllFamous()
+        {
+            var blogs = await _blogRepository.GetAllFamousAsync();
+            return Ok(blogs);
+        }
+
+        [Authorize]
+        [HttpDelete("{blogId}")]
+        public async Task<ActionResult<int>> Delete(int blogId)
+        {
+            var applicationUserId = int.Parse(User.Claims.First(i => i.Type == JwtRegisteredClaimNames.NameId).Value);
+
+            var foundBlog = await _blogRepository.GetAsync(blogId);
+            if(foundBlog == null)
+            {
+                return NotFound("Blog does not exists");
+            }
+            if(foundBlog.ApplicationUserId != applicationUserId)
+            { 
+                return Unauthorized("You are not authorized to delete this blog");
+            }
+            
+            var affectedRows = await _blogRepository.DeleteAsync(blogId);
+            return Ok(affectedRows);
+        }
+
+    }
+}
