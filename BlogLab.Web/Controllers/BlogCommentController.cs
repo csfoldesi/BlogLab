ebed01c06299681b59f1345ce64a7e3c0769ca94
Csaba Földesi <csabus@gmail.com>
2023-03-11 22:53:25 +0100

    BlogCommentController created

---
+++ BlogLab.Web/Controllers/BlogCommentController.cs
@@ -0,0 +1,59 @@
+﻿using BlogLab.Models.BlogComment;
+using BlogLab.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace BlogLab.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogCommentController : ControllerBase
+    {
+        private readonly IBlogCommentRepository _blogCommentRepository;
+
+        public BlogCommentController(IBlogCommentRepository blogCommentRepository)
+        {
+            _blogCommentRepository = blogCommentRepository;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<BlogComment>> Create(BlogCommentCreate blogCommentCreate)
+        {
+            var applicationUserId = int.Parse(User.Claims.First(i => i.Type == JwtRegisteredClaimNames.NameId).Value);
+
+            var createdBlogComment = await _blogCommentRepository.UpsertAsync(blogCommentCreate, applicationUserId);
+            return Ok(createdBlogComment);
+        }
+
+        [HttpGet("{blogId}")]
+        public async Task<ActionResult<List<BlogComment>>> GetAll(int blogId)
+        {
+            var blogComments = await _blogCommentRepository.GetAllAsync(blogId);
+            return Ok(blogComments);
+        }
+
+        [Authorize]
+        [HttpDelete("{blogCommentId}")]
+        public async Task<ActionResult<int>> Delete(int blogCommentId)
+        {
+            var applicationUserId = int.Parse(User.Claims.First(i => i.Type == JwtRegisteredClaimNames.NameId).Value);
+
+            var foundBlogComment = await _blogCommentRepository.GetAsync(blogCommentId);
+            if (foundBlogComment == null)
+            {
+                return NotFound("Blog comment not found");
+            }
+
+            if(foundBlogComment.ApplicationUserId != applicationUserId)
+            {
+                return Unauthorized("You do not have permission to delete this blog comment");
+            }
+
+            var affectedRows = await _blogCommentRepository.DeleteAsync(blogCommentId);
+            return Ok(affectedRows);
+        }
+
+    }
+}
