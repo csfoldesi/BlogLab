201a09e2d102bef5e20e3388b024f4ef7b4a75cd
Csaba Földesi <csabus@gmail.com>
2023-03-15 15:58:49 +0100

    [Frontend] - JwtInterceptor created

--- BlogLab-UI/src/app/interceptors/jwt.interceptor.ts
+++ BlogLab-UI/src/app/interceptors/jwt.interceptor.ts
@@ -6,15 +6,28 @@ import {
   HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AccountService } from '../services/account.service';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private accountService: AccountService) {}
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
+    const currentUser = this.accountService.currentUserValue;
+    const isLoggedIn = !!currentUser && !!currentUser.token;
+    const isApiUrl = request.url.startsWith(environment.webApi);
+
+    if (isApiUrl && isLoggedIn) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearet ${currentUser.token}`,
+        },
+      });
+    }
     return next.handle(request);
   }
 }
