f7b7cc5ffbdebc18b3fe51bfdaae6984a4ce7d84
Csaba Földesi <csabus@gmail.com>
2023-03-20 22:14:56 +0100

    [Frontend] - AuthGuard implemented

--- BlogLab-UI/src/app/guards/auth.guard.ts
+++ BlogLab-UI/src/app/guards/auth.guard.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AccountService } from '../services/account.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+  constructor(private accountSercice: AccountService, private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -19,6 +23,11 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
+    if (this.accountSercice.isLoggedIn()) {
+      return true;
+    }
+
+    this.router.navigate(['/']);
     return true;
   }
 }
