22d8b404711286b29fd2c84f04e54111b6b0a8aa
Csaba Földesi <csabus@gmail.com>
2023-03-22 20:59:14 +0100

    [Frontend] - NavbarComponent implemented

--- BlogLab-UI/src/app/components/navbar/navbar.component.ts
+++ BlogLab-UI/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from '../../services/account.service';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+    isCollapsed = true;
 
+    constructor(
+        public accountService: AccountService,
+        private router: Router
+    ) {}
+
+    logout(): void {
+        this.accountService.logout();
+        this.router.navigate(['/']);
+    }
 }
