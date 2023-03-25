d08936cae5d8a145d48da12e3ff3cbb42bad9491
Csaba Földesi <csabus@gmail.com>
2023-03-25 23:21:23 +0100

    [Frontend] - Authorization fix in JwtInterceptor

--- BlogLab-UI/src/app/interceptors/jwt.interceptor.ts
+++ BlogLab-UI/src/app/interceptors/jwt.interceptor.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AccountService } from '../services/account.service';
@@ -11,22 +11,24 @@ import { environment } from '../../environments/environment';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  constructor(private accountService: AccountService) {}
+    constructor(private accountService: AccountService) {}
 
-  intercept(
-    request: HttpRequest<unknown>,
-    next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
-    const currentUser = this.accountService.currentUserValue;
-    const isApiUrl = request.url.startsWith(environment.webApi);
+    intercept(
+        request: HttpRequest<unknown>,
+        next: HttpHandler
+    ): Observable<HttpEvent<unknown>> {
+        const currentUser = this.accountService.currentUserValue;
+        const isApiUrl = request.url.startsWith(environment.webApi);
 
-    if (isApiUrl && this.accountService.isLoggedIn()) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearet ${currentUser ? currentUser.token : ''}`,
-        },
-      });
+        if (isApiUrl && this.accountService.isLoggedIn()) {
+            request = request.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${
+                        currentUser ? currentUser.token : ''
+                    }`,
+                },
+            });
+        }
+        return next.handle(request);
     }
-    return next.handle(request);
-  }
 }
