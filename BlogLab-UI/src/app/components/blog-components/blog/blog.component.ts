71c7195dad8828b6cfc982dc4ecc8d3e403f3845
Csaba Földesi <csabus@gmail.com>
2023-03-30 22:55:21 +0200

    [Frontend] - BlogComponent implemented

--- BlogLab-UI/src/app/components/blog-components/blog/blog.component.ts
+++ BlogLab-UI/src/app/components/blog-components/blog/blog.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Blog } from '../../../models/blog/blog.model';
+import { BlogService } from '../../../services/blog.service';
+import { PhotoService } from '../../../services/photo.service';
 
 @Component({
-  selector: 'app-blog',
-  templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+    selector: 'app-blog',
+    templateUrl: './blog.component.html',
+    styleUrls: ['./blog.component.css'],
 })
-export class BlogComponent {
+export class BlogComponent implements OnInit {
+    blog?: Blog;
 
+    blogPhotoUrl?: string;
+
+    constructor(
+        private route: ActivatedRoute,
+        private blogService: BlogService,
+        private photoService: PhotoService
+    ) {}
+
+    ngOnInit(): void {
+        const blogId = parseInt(this.route.snapshot.paramMap.get('id') ?? '0');
+
+        this.blogService.get(blogId).subscribe((blog) => {
+            this.blog = blog;
+
+            if (this.blog?.photoId) {
+                this.photoService.get(this.blog.photoId).subscribe((photo) => {
+                    this.blogPhotoUrl = photo.imageUrl;
+                });
+            }
+        });
+    }
 }
