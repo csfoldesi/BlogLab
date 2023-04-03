c6b3aa39ab3dba6ad598604a7e0910f1f4d2c102
Csaba Földesi <csabus@gmail.com>
2023-04-03 23:09:29 +0200

    [Frontend] - CommentSystemComponent implemented

--- BlogLab-UI/src/app/components/comment-components/comment-system/comment-system.component.ts
+++ BlogLab-UI/src/app/components/comment-components/comment-system/comment-system.component.ts
@@ -1,10 +1,115 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BlogCommentService } from '../../../services/blog-comment.service';
+import { AccountService } from '../../../services/account.service';
+import { BlogCommentViewModel } from '../../../models/blog-comment/blog-comment-view-model.model';
+import { BlogComment } from '../../../models/blog-comment/blog-comment.model';
 
 @Component({
-  selector: 'app-comment-system',
-  templateUrl: './comment-system.component.html',
-  styleUrls: ['./comment-system.component.css']
+    selector: 'app-comment-system',
+    templateUrl: './comment-system.component.html',
+    styleUrls: ['./comment-system.component.css'],
 })
-export class CommentSystemComponent {
+export class CommentSystemComponent implements OnInit {
+    @Input() blogId!: number;
 
+    standaloneComment?: BlogCommentViewModel;
+
+    blogComments: BlogComment[] = [];
+
+    blogCommentViewModels: BlogCommentViewModel[] = [];
+
+    constructor(
+        private blogCommentService: BlogCommentService,
+        public accountService: AccountService
+    ) {}
+
+    ngOnInit(): void {
+        this.blogCommentService
+            .getAll(this.blogId)
+            .subscribe((blogComments) => {
+                if (this.accountService.isLoggedIn()) {
+                    this.initComment(
+                        this.accountService.currentUserValue?.username || ''
+                    );
+                }
+
+                this.blogComments = blogComments;
+                this.blogCommentViewModels = [];
+
+                this.blogComments.forEach((blogComment) => {
+                    if (!blogComment.parentBlogCommentId) {
+                        this.findCommentReplies(
+                            this.blogCommentViewModels,
+                            blogComment
+                        );
+                    }
+                });
+            });
+    }
+
+    initComment(username: string): void {
+        this.standaloneComment = {
+            parentBlogCommentId: null,
+            blogCommentId: -1,
+            blogId: this.blogId,
+            content: '',
+            username,
+            publishDate: null,
+            updateDate: null,
+            isEditable: false,
+            deleteConfirm: false,
+            isReplying: false,
+            comments: [],
+        };
+    }
+
+    findCommentReplies(
+        blogCommentViewModels: BlogCommentViewModel[],
+        blogComment: BlogComment
+    ): void {
+        const newComments: BlogCommentViewModel[] = [];
+
+        const commentViewModel: BlogCommentViewModel = {
+            parentBlogCommentId: blogComment.parentBlogCommentId || null,
+            blogCommentId: blogComment.blogCommentId,
+            blogId: blogComment.blogId,
+            content: blogComment.content,
+            username: blogComment.username,
+            publishDate: blogComment.publishDate,
+            updateDate: blogComment.updateDate,
+            isEditable: false,
+            deleteConfirm: false,
+            isReplying: false,
+            comments: newComments,
+        };
+
+        blogCommentViewModels.push(commentViewModel);
+
+        this.blogComments.forEach((bc) => {
+            if (bc.parentBlogCommentId === blogComment.blogCommentId) {
+                this.findCommentReplies(newComments, bc);
+            }
+        });
+    }
+
+    onCommentSaved(blogComment: BlogComment): void {
+        const commentViewModel: BlogCommentViewModel = {
+            parentBlogCommentId: blogComment.parentBlogCommentId || null,
+            blogCommentId: blogComment.blogCommentId,
+            blogId: blogComment.blogId,
+            content: blogComment.content,
+            username: blogComment.username,
+            publishDate: blogComment.publishDate,
+            updateDate: blogComment.updateDate,
+            isEditable: false,
+            deleteConfirm: false,
+            isReplying: false,
+            comments: [],
+        };
+
+        this.blogCommentViewModels = [
+            commentViewModel,
+            ...this.blogCommentViewModels,
+        ];
+    }
 }
--- BlogLab-UI/src/app/models/blog-comment/blog-comment-view-model.model.ts
+++ BlogLab-UI/src/app/models/blog-comment/blog-comment-view-model.model.ts
@@ -1,17 +1,15 @@
 export class BlogCommentViewModel {
-    
     constructor(
-        public parentBlogCommentId: number,
+        public parentBlogCommentId: number | null,
         public blogCommentId: number,
         public blogId: number,
         public content: string,
         public username: string,
-        public applicationUserId: number,
-        public publishDate: Date,
-        public updateDate: Date,
+        public publishDate: Date | null,
+        public updateDate: Date | null,
         public isEditable: boolean = false,
         public deleteConfirm: boolean = false,
         public isReplying: boolean = false,
-        public comments: BlogCommentViewModel[]
-    ) { }
+        public comments: BlogCommentViewModel[] = []
+    ) {}
 }
