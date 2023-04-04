34108319a97d4144d91d3831b13022ded1ce88ad
Csaba Földesi <csabus@gmail.com>
2023-04-04 20:32:26 +0200

    [Frontend] - CommentBoxComponent implemented

--- BlogLab-UI/src/app/components/comment-components/comment-box/comment-box.component.ts
+++ BlogLab-UI/src/app/components/comment-components/comment-box/comment-box.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output,
+    ViewChild,
+} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { BlogCommentCreate } from '../../../models/blog-comment/blog-comment-create.model';
+import { BlogCommentService } from '../../../services/blog-comment.service';
+import { BlogCommentViewModel } from '../../../models/blog-comment/blog-comment-view-model.model';
+import { BlogComment } from '../../../models/blog-comment/blog-comment.model';
 
 @Component({
-  selector: 'app-comment-box',
-  templateUrl: './comment-box.component.html',
-  styleUrls: ['./comment-box.component.css']
+    selector: 'app-comment-box',
+    templateUrl: './comment-box.component.html',
+    styleUrls: ['./comment-box.component.css'],
 })
-export class CommentBoxComponent {
+export class CommentBoxComponent implements OnInit {
+    @Input() comment!: BlogCommentViewModel;
 
+    @Output() commentSaved = new EventEmitter<BlogComment>();
+
+    @ViewChild('commentForm') commentForm?: NgForm;
+
+    constructor(
+        private blogCommentService: BlogCommentService,
+        private toastr: ToastrService
+    ) {}
+
+    ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
+
+    resetComment(): void {
+        this.commentForm?.reset();
+    }
+
+    onSubmit(): void {
+        const blogCommentCreate: BlogCommentCreate = {
+            blogCommentId: this.comment.blogCommentId,
+            parentBlogCommentId: this.comment.parentBlogCommentId,
+            blogId: this.comment.blogId,
+            content: this.comment.content,
+        };
+
+        this.blogCommentService
+            .create(blogCommentCreate)
+            .subscribe((blogComment) => {
+                this.commentSaved.emit(blogComment);
+                this.resetComment();
+                this.toastr.info('Comment saved');
+            });
+    }
 }
